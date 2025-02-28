cd441b5b3113320849a44817b763208a0b965b1c
Nelly <nelly.babillon@gmail.com>
2025-02-28 13:52:39 +0100

    Ajout APIResponse

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,17 +11,29 @@ import { ProductsService } from './products.service';
 import { Product } from './product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createCatDto: CreateProductDto): Promise<Product> {
+  @ApiResponse({
+    status: 201,
+    description: 'Le produit a été créé avec succès.',
+  })
+  @ApiResponse({ status: 403, description: "Vous n'avez pas l'autorisation" })
+  async create(@Body() createCatDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createCatDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Tous les produits ont été récupérés avec succès.',
+  })
+  @ApiResponse({ status: 403, description: "Vous n'avez pas l'autorisation" })
   findAll(): Promise<Product[]> {
     return this.productsService.findAll();
   }
