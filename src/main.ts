38c16a76216f68b71881cdd464d28a4504bfd83e
Nelly <nelly.babillon@gmail.com>
2025-02-28 13:27:31 +0100

    install Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Gestion des commandes')
+    .setDescription('API des commandes')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3300);
 }
 bootstrap();
